bd9f6c354e8effa6e9e33d945d010aee76dbdf34
Steve Mostovoy <stevemostovoysm@gmail.com>
2016-12-18 21:28:18 -0800

    Run eval in subshell to handle fail fasts.

--- commands/lib.sh
+++ commands/lib.sh
@@ -85,7 +85,7 @@ async() {
         pid=$(bash -c 'echo $PPID')
         out=$(out_file $pid)
         rm -f $out
-        if ! eval $@ > $out 2>&1; then
+        if ! (eval $@ > $out 2>&1); then
             touch $(failed_file $pid)
         fi
     )&
